c1a4787edd3e2d9bed66218dcb48b76dfe9d06d0
Damien Churchill <damien.churchill@ukplc.net>
2011-06-10 16:49:08 +0100

    initial support for async tracking
    Add initial support for loading the tracking code from piwik
    using asynchronous methods which will stop the page being blocked
    on load making it appear as if it is taking longer to load. Async
    is fun.

--- piwik.php
+++ piwik.php
@@ -38,22 +38,46 @@ class piwik extends rcube_plugin {
 
 		$piwik_site_id = $rcmail->config->get('piwik_site_id');
 		$piwik_url = $rcmail->config->get('piwik_url');
+		$piwik_async = $rcmail->config->get('piwik_async');
 		$set_domain_name = '';
 
 		if (!empty($piwik_url) && !empty($piwik_site_id)) {
-			$script = '
-			<!-- Piwik -->
-			<script type="text/javascript">
-			var pkBaseURL = (("https:" == document.location.protocol) ? "https://' . $piwik_url . '/" : "http://' . $piwik_url . '/");
-			document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-			</script><script type="text/javascript">
-			try {
-			var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", ' . $piwik_site_id . ');
-			piwikTracker.trackPageView();
-			piwikTracker.enableLinkTracking();
-			} catch( err ) {}
-			</script><noscript><p><img src="http://' . $piwik_url . '/piwik.php?idsite=' . $piwik_site_id .'" style="border:0" alt="" /></p></noscript>
-			<!-- End Piwik Tracking Tag -->';
+			if ($piwik_async) {
+				$srcipt = "
+				<!-- Piwik -->
+				<script type='text/javascript'>
+				var _paq = _paq || [];
+				(function(){
+					var u=(('https:' == document.location.protocol) ? 'https://$piwik_url' : 'http://$piwik_url');
+					_paq.push(['setSiteId', $piwik_site_id]);
+					_paq.push(['setTrackerUrl', u+'piwik.php']);
+					_paq.push(['trackPageView']);
+					_paq.push(['enableLinkTracking']);
+					var d=document,
+						g=d.createElement('script'),
+						s=d.getElementsByTagName('script')[0];
+						g.type='text/javascript';
+						g.defer=true;
+						g.async=true;
+						g.src=u+'piwik.js';
+						s.parentNode.insertBefore(g,s);
+				})();
+				<!-- End Piwik Tracking Tag -->";
+			} else {
+				$script = "
+				<!-- Piwik -->
+				<script type='text/javascript'>
+				var pkBaseURL = (('https:' == document.location.protocol) ? 'https://$piwik_url/' : 'http://$piwik_url');
+				document.write(unescape('%3Cscript src=\"' + pkBaseURL + 'piwik.js\" type=\"text/javascript\"%3E%3C/script%3E'));
+				</script><script type='text/javascript'>
+				try {
+				var piwikTracker = Piwik.getTracker(pkBaseURL + 'piwik.php', $piwik_site_id);
+				piwikTracker.trackPageView();
+				piwikTracker.enableLinkTracking();
+				} catch( err ) {}
+				</script><noscript><p><img src='http://$piwik_url/piwik.php?idsite=$piwik_site_id' style='border:0' alt='' /></p></noscript>
+				<!-- End Piwik Tracking Tag -->";
+			}
 
 			$rcmail->output->add_footer($script);
 		}
